3c5628b7ad7cf2f92101efdaaf6ac361819abdd8
Sylvio Maggessi <sylviodev@gmail.com>
2021-10-01 01:51:35 -0300

    first commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,28 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not exists.");
+    }
+
+    if (!user.admin) {
+      throw new Error("User dont have permission for this action.");
+    }
+
+    const users: User[] = this.usersRepository.list();
+
+    return users;
+  }
+}
+
+export { ListAllUsersUseCase };
---
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
+
+class TurnUserAdminController {
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    try {
+      const { user_id } = request.params;
+
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.json(user);
+    } catch (err) {
+      const result = (err as Error).message;
+      return response.status(404).json({ error: result });
+    }
+  }
+}
+
+export { TurnUserAdminController };
